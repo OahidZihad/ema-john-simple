b569606a08fbb639686748dd5321dbacd34b2bf7
OahidZihad <oahidzihad1@gmail.com>
2021-05-05 02:36:05 +0600

    Dynamic path, set route path parameter

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import './Product.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./Product.css";
+import { Link } from "react-router-dom";
 
 const Product = (props) => {
-    console.log(props);
+  console.log(props.product.key);
 
-    const {img, name, seller, price, stock} = props.product;
-    return (
-        <div className="product">
-            <div>
-                <img src={img} alt=""/>
-            </div>
-            <div>
-                <h4 className="product-name text-primary">{name}</h4>
-                <p><small>By: {seller}</small></p>
-                <p>${price}</p>
-                <p><small>Only {stock} left in stock - Order Soon</small></p>
-                <button className="main-button" onClick={() => props.handleAddProduct(props.product)}> 
-                    <FontAwesomeIcon icon={faShoppingCart} /> add to cart
-                </button>
-            </div>
-        </div>
-    );
+  const { img, name, seller, price, stock, key } = props.product;
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <h4 className="product-name text-primary">
+          <Link to={"/product/" + key}>{name}</Link>
+        </h4>
+        <p>
+          <small>By: {seller}</small>
+        </p>
+        <p>${price}</p>
+        <p>
+          <small>Only {stock} left in stock - Order Soon</small>
+        </p>
+        <button
+          className="main-button"
+          onClick={() => props.handleAddProduct(props.product)}
+        >
+          <FontAwesomeIcon icon={faShoppingCart} /> add to cart
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
---
+++ src/components/Review/Review.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Review = () => {
+  return (
+    <div>
+      <h1>This is REview</h1>
+    </div>
+  );
+};
+
+export default Review;
