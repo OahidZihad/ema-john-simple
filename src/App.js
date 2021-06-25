8f1f990e6db15ccb0b608097565314349ea866df
OahidZihad <oahidzihad1@gmail.com>
2021-06-25 07:58:42 +0600

    Create private route and set auth redirect

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ProductDetail from "./components/ProductDetail/ProductDetail";
 import Shipment from "./components/Shipment/Shipment";
 import Login from "./components/Login/Login";
 import { createContext, useState } from "react";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
 export const UserContext = createContext();
 
@@ -26,15 +27,15 @@ function App() {
           <Route path="/review">
             <Review></Review>
           </Route>
-          <Route path="/inventory">
+          <PrivateRoute path="/inventory">
             <Inventory></Inventory>
-          </Route>
+          </PrivateRoute>
           <Route path="/login">
             <Login></Login>
           </Route>
-          <Route path="/shipment">
+          <PrivateRoute path="/shipment">
             <Shipment></Shipment>
-          </Route>
+          </PrivateRoute>
           <Route exact path="/">
             <Shop></Shop>
           </Route>
