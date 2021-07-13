d12c9abbdef2dfb5b0734629cdb32fe0aa08df42
OahidZihad <oahidzihad1@gmail.com>
2021-07-13 21:23:04 +0600

    Save orders in a new data collection in mongodb

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { UserContext } from "../../App";
+import { getDatabaseCart, processOrder } from "../../utilities/databaseManager";
 import "./Shipment.css";
 
 const Shipment = () => {
@@ -12,7 +13,28 @@ const Shipment = () => {
   } = useForm();
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   const onSubmit = (data) => {
-    console.log("form submitted", data);
+    const savedCart = getDatabaseCart();
+    const orderDetails = {
+      ...loggedInUser,
+      products: savedCart,
+      shipment: data,
+      orderTime: new Date(),
+    };
+    fetch("http://localhost:4000/addOrder", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(orderDetails),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          console.log(data);
+          processOrder();
+          alert("your order placed successfully");
+        }
+      });
   };
 
   console.log(watch("example")); // watch input value by passing the name of it
