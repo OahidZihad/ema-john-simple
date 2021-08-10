6fb1982b25fc52c396be83e47ea451f3c7b526fb
OahidZihad <oahidzihad1@gmail.com>
2021-08-10 12:46:21 +0600

    create payment process component with simple card form using stripe payment getway

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { UserContext } from "../../App";
 import { getDatabaseCart, processOrder } from "../../utilities/databaseManager";
+import ProcessPayment from "../ProcessPayment/ProcessPayment";
 import "./Shipment.css";
 
 const Shipment = () => {
@@ -40,36 +41,46 @@ const Shipment = () => {
   console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-    <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        defaultValue={loggedInUser.name}
-        {...register("name", { required: true })}
-        placeholder="Your Name"
-      />
-      {errors.name && <span className="error">Name is required</span>}
-      <input
-        defaultValue={loggedInUser.email}
-        {...register("email", { required: true })}
-        placeholder="Your Email Address"
-      />
-      {errors.email && <span className="error">Email is required</span>}
-      <input
-        {...register("district", { required: true })}
-        placeholder="Your District"
-      />
-      {errors.district && <span className="error">District is required</span>}
-      <input
-        {...register("address", { required: true })}
-        placeholder="Your Full Address"
-      />
-      {errors.address && <span className="error">Address is required</span>}
-      <input
-        {...register("phone", { required: true })}
-        placeholder="Your Phone Number"
-      />
-      {errors.phone && <span className="error">Phone is required</span>}
-      <input type="submit" />
-    </form>
+    <div className="row">
+      <div className="col-md-6">
+        <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            defaultValue={loggedInUser.name}
+            {...register("name", { required: true })}
+            placeholder="Your Name"
+          />
+          {errors.name && <span className="error">Name is required</span>}
+          <input
+            defaultValue={loggedInUser.email}
+            {...register("email", { required: true })}
+            placeholder="Your Email Address"
+          />
+          {errors.email && <span className="error">Email is required</span>}
+          <input
+            {...register("district", { required: true })}
+            placeholder="Your District"
+          />
+          {errors.district && (
+            <span className="error">District is required</span>
+          )}
+          <input
+            {...register("address", { required: true })}
+            placeholder="Your Full Address"
+          />
+          {errors.address && <span className="error">Address is required</span>}
+          <input
+            {...register("phone", { required: true })}
+            placeholder="Your Phone Number"
+          />
+          {errors.phone && <span className="error">Phone is required</span>}
+          <input type="submit" />
+        </form>
+      </div>
+      <div className="col-md-6">
+        <h2>Plase Pay....</h2>
+        <ProcessPayment></ProcessPayment>
+      </div>
+    </div>
   );
 };
 
