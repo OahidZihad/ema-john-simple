e8e41a7239f4fa56fc6abab9767172e4e40a83b5
OahidZihad <oahidzihad1@gmail.com>
2022-03-05 23:07:21 +0600

    test and phitron.io site design practice

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,6 +15,7 @@ const Header = () => {
         <Link to="/shop">Shop</Link>
         <Link to="/review">Order Review</Link>
         <Link to="/inventory">Manage Inventory</Link>
+        <Link to="/test">Test</Link>
         {loggedInUser.name ? (
           <button onClick={() => setLoggedInUser({})}>Sign Out</button>
         ) : (
